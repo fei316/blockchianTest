41c13276c71159602c874cb27d127ccbadb6f408
zhoutianing <zhoutianxing@live.com>
2019-05-26 17:41:15 +0800

    v1 done

--- block.go
+++ block.go
@@ -1,28 +1,70 @@
 package main
 
-import "crypto/sha256"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"time"
+)
 
 //区块结构体
 type Block struct {
+	Version uint64
+
 	PrevHash []byte
-	Hash     []byte
-	Data     []byte
+
+	MerkelRoot []byte
+
+	TimeStamp uint64
+
+	Difficulty uint64
+
+	Nonce uint64
+
+	//hash和data，正常情况布不存储在这里，存储在这里是为了实现方便
+	Hash []byte
+	Data []byte
 }
 
 //创建区块
 func NewBloack(data string, prevHash []byte) *Block {
 	block := Block{
-		PrevHash: prevHash,
-		Hash:     []byte{},
-		Data:     []byte(data),
+		Version:    00,
+		PrevHash:   prevHash,
+		MerkelRoot: []byte{},
+		TimeStamp:  uint64(time.Now().Unix()),
+		Difficulty: 0,
+		Nonce:      0,
+		Hash:       []byte{},
+		Data:       []byte(data),
 	}
 	block.SetHash()
 	return &block
 }
 
+//uint转[]byte
+func Unit64ToByte(num uint64) []byte {
+	var buf = make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, uint64(num))
+	return buf
+}
+
 //给区块生成hash
 func (block *Block) SetHash() {
-	blockInfo := append(block.PrevHash, block.Data...)
+
+	tmp := [][]byte{
+		Unit64ToByte(block.Version),
+		block.PrevHash,
+		block.MerkelRoot,
+		Unit64ToByte(block.TimeStamp),
+		Unit64ToByte(block.Difficulty),
+		Unit64ToByte(block.Nonce),
+		block.Data,
+	}
+
+	blockInfo := bytes.Join(tmp, []byte{})
+
+
 	hash := sha256.Sum256(blockInfo)
 	block.Hash = hash[:]
 }
