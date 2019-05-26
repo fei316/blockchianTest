cdd6ff84888bd765825063b2c0c7d336a64a2c74
zhoutianing <zhoutianxing@live.com>
2019-05-26 11:47:58 +0800

    创世区块创建

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-//区块链结构体
+//区块结构体
 type Block struct {
 	PrevHash []byte
 	Hash     []byte
@@ -23,15 +23,38 @@ func NewBloack(data string, prevHash []byte) *Block {
 	return &block
 }
 
+//给区块生成hash
 func (block *Block) SetHash() {
 	blockInfo := append(block.PrevHash, block.Data...)
 	hash := sha256.Sum256(blockInfo)
 	block.Hash = hash[:]
 }
 
+//区块链结构体
+type BlockChian struct {
+	blocks []Block
+}
+
+func GenesisBlock() *Block {
+	block := NewBloack("我的创世区块2019年5月26日", []byte{})
+	return block
+}
+
+//创建区块链
+func NewBlockchian() *BlockChian {
+	blockchian := BlockChian{
+		blocks:[]Block{*GenesisBlock()},
+	}
+	return &blockchian
+}
+
 func main() {
-	block := NewBloack("小明向小红转了10个比特币", []byte{})
-	fmt.Printf("前Hash：%x\n", block.PrevHash)
-	fmt.Printf("当前Hash：%x\n", block.Hash)
-	fmt.Printf("数据：%s\n", block.Data)
+	bc := NewBlockchian()
+	for i, block := range bc.blocks {
+		fmt.Printf("=====当前区块高度：%d =======\n", i)
+		fmt.Printf("前Hash：%x\n", block.PrevHash)
+		fmt.Printf("当前Hash：%x\n", block.Hash)
+		fmt.Printf("数据：%s\n", block.Data)
+	}
+
 }
