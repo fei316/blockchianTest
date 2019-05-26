3b8e368f338e232d94ed051882dd71ce3991c932
zhoutianing <zhoutianxing@live.com>
2019-05-26 18:26:48 +0800

    create pow

---
+++ proofofwork.go
@@ -0,0 +1,19 @@
+package main
+
+import "math/big"
+
+type ProofOfWork struct {
+	block *Block
+	target *big.Int
+}
+
+func NewProofOfWork(block *Block) *ProofOfWork {
+	pf := ProofOfWork{
+		block:block,
+	}
+	targetStr := "0001000000000000000000000000000000000000000000000000000000000000"
+	tmpbig := big.Int{}
+	tmpbig.SetString(targetStr, 16)
+	pf.target = &tmpbig
+	return &pf
+}
